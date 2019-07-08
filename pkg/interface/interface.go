2b0efd7d0d0ed2303364a50037277697133ab814
Janrajc <janrajcj@gmail.com>
2019-07-08 18:42:24 +0530

    Creating CI stages for MIC

--- pkg/interface/interface.go
+++ pkg/interface/interface.go
@@ -8,7 +8,7 @@ import (
         "net/http"
         "time"
 	"fmt"
-	ctr "multicluster-ingress-controller/pkg/controller"
+	ctr "github.com/janraj/multicluster-ingress-controller/pkg/controller"
 	"os"
 )
 
